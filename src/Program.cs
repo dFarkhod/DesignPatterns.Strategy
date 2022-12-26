71744f36954956b7e8f54e0cf8d1dc7d543d9440
Farkhod <dfarkhod@gmail.com>
2022-12-26 18:38:08 +0800

    updates

---
+++ src/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Reflection;
+
+namespace Virtualdars.DesignPatterns.Strategy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // 1-misol:
+            Context context;
+
+            context = new Context(new ConcreteStrategyA());
+            context.RunStrategy();
+
+            context = new Context(new ConcreteStrategyB());
+            context.RunStrategy();
+            Console.WriteLine();
+
+
+            // 2-misol:
+            var staff = new Staff { Name = "Umar", Level = EngineerLevel.Lead, BaseSalary = 10000, NumberOfSubordinates = 5, NumberOfProjectsDelivered = 9, YearsOfExperience = 8 };
+            var engineer = new SoftwareEngineer(staff);
+            var salary = engineer.GetMonthlySalary();
+            Console.WriteLine(staff.Name + ":" + salary);
+
+            var staff2 = new Staff { Name = "Aziz", Level = EngineerLevel.Middle, BaseSalary = 6000, NumberOfSubordinates = 2, NumberOfProjectsDelivered = 4, YearsOfExperience = 3 };
+            var engineer2 = new SoftwareEngineer(staff2);
+            var salary2 = engineer2.GetMonthlySalary();
+            Console.WriteLine(staff2.Name + ":" + salary2);
+
+            Console.ReadLine();
+        }
+    }
+
+    public interface IStrategy
+    {
+        public void AlgorithmStrategy();
+    }
+
+
+    public class Context
+    {
+        private IStrategy _strategy;
+
+        public Context(IStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public void RunStrategy()
+        {
+            _strategy.AlgorithmStrategy();
+        }
+    }
+
+
+    public class ConcreteStrategyA : IStrategy
+    {
+        public void AlgorithmStrategy()
+        {
+            Console.WriteLine(GetType().Name + "." + MethodBase.GetCurrentMethod().Name);
+        }
+    }
+
+    public class ConcreteStrategyB : IStrategy
+    {
+        public void AlgorithmStrategy()
+        {
+            Console.WriteLine(GetType().Name + "." + MethodBase.GetCurrentMethod().Name);
+        }
+    }
+
+
+    public class ConcreteStrategyC : IStrategy
+    {
+        public void AlgorithmStrategy()
+        {
+            Console.WriteLine(GetType().Name + "." + MethodBase.GetCurrentMethod().Name);
+        }
+    }
+
+}
---
+++ src/SoftwareEngineer.cs
@@ -0,0 +1,104 @@
+﻿namespace Virtualdars.DesignPatterns.Strategy
+{
+    public class SoftwareEngineer
+    {
+        private Engineer _engineer;
+
+        public SoftwareEngineer(Staff staff)
+        {
+            _engineer = Engineer.Factory.Create(staff);
+        }
+
+        public decimal GetMonthlySalary()
+        {
+            return _engineer.GetMonthlySalary();
+        }
+    }
+
+    public class Staff
+    {
+        public string Name;
+        public EngineerLevel Level;
+        public int NumberOfSubordinates;
+        public int YearsOfExperience;
+        public int NumberOfProjectsDelivered;
+        public decimal BaseSalary;
+    }
+
+    public enum EngineerLevel
+    {
+        Intern,
+        Junior,
+        Middle,
+        Senior,
+        Lead
+    }
+
+    public abstract class Engineer
+    {
+        public Staff Staff;
+
+        public static class Factory
+        {
+            public static Engineer Create(Staff staff)
+            {
+                switch (staff.Level)
+                {
+                    case EngineerLevel.Intern:
+                        return new InternEngineer { Staff = staff };
+                    case EngineerLevel.Junior:
+                        return new JuniorEngineer { Staff = staff };
+                    case EngineerLevel.Middle:
+                        return new MiddleEngineer { Staff = staff };
+                    case EngineerLevel.Senior:
+                        return new SeniorEngineer { Staff = staff };
+                    case EngineerLevel.Lead:
+                        return new LeadEngineer { Staff = staff };
+                    default:
+                        throw new NotImplementedException("Bunday toifa mavjud emas");
+                }
+            }
+        }
+        public abstract decimal GetMonthlySalary();
+    }
+
+    public class InternEngineer : Engineer
+    {
+        public override decimal GetMonthlySalary()
+        {
+            return Staff.BaseSalary;
+        }
+    }
+
+    public class JuniorEngineer : Engineer
+    {
+        public override decimal GetMonthlySalary()
+        {
+            return Staff.BaseSalary;
+        }
+    }
+
+    public class MiddleEngineer : Engineer
+    {
+        public override decimal GetMonthlySalary()
+        {
+            return Staff.BaseSalary * 1.1M + (decimal)(Staff.NumberOfSubordinates * 0.03M * Staff.BaseSalary);
+        }
+    }
+
+    public class SeniorEngineer : Engineer
+    {
+        public override decimal GetMonthlySalary()
+        {
+            return (Staff.BaseSalary * 1.25M) + (decimal)(Staff.YearsOfExperience * 0.01M * Staff.BaseSalary) + (decimal)(Staff.NumberOfProjectsDelivered * 0.02M * Staff.BaseSalary + (decimal)(Staff.NumberOfSubordinates * 0.03M * Staff.BaseSalary));
+        }
+    }
+
+    public class LeadEngineer : Engineer
+    {
+        public override decimal GetMonthlySalary()
+        {
+            return (Staff.BaseSalary * 1.25M) + (decimal)(Staff.YearsOfExperience * 0.01M * Staff.BaseSalary) + (decimal)(Staff.NumberOfProjectsDelivered * 0.03M * Staff.BaseSalary) + (decimal)(Staff.NumberOfSubordinates * 0.05M * Staff.BaseSalary);
+        }
+    }
+}
